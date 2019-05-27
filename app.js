80b3dd7f4a46f90db212f980a1232860f396bf9a
Advanced3 <david.schmidt382001@gmail.com>
2019-05-27 17:36:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ function again(){
 var questions = [
     new Question("Ktoré z týchto intravenóznych anestetík nepatrí medzi barbiturátové?",["Thiopental","Metohexidal","Pentobarbital","Ketamín"],"Ketamín"),
     new Question("Aké množstvo propofolu sa používa na uvedenie do celkovej anestézy?",["1 mg/kg","2,5 mg/kg","9 mg/kg","tak kilo a pol"],"2,5 mg/kg"),
-    new Question("Ktorá kyselina je najsilnejšia na svete?",["H2S04","HCl","metanol z Ľetanovec","HSbF6"],"HSbF6"),
-    new Question("Aká je smrteľná dávka radiácie?",["das 2 a pol deci","0,5 Sv","500 röntgenov počas 5 hodín","dinosaurus"],"500 röntgenov počas 5 hodín"),
-    new Question("Koľko energie sa uvoľní z 1 gramu Uránu 235?",["10000 cal","75600 MJ","3 MWh","ta dosc na švecene 4 žiaroviek"],"75600 MJ"),
-    new Question("Koľko žalúdkov má krava?",["1","3","4","A ja znam?"],"4"),
+    new Question("Ktorá kyselina je najsilnejšia na svete?",["H2S04","HCl","H20","HSbF6"],"HSbF6"),
+    new Question("Aká je smrteľná dávka radiácie?",["tak asi 2 a pol deci","0,5 Sv","500 röntgenov počas 5 hodín","dinosaurus"],"500 röntgenov počas 5 hodín"),
+    new Question("Koľko energie sa uvoľní z 1 gramu Uránu 235?",["10000 cal","75600 MJ","3 MWh","dosť na 4 žiarovky"],"75600 MJ"),
+    new Question("Koľko žalúdkov má krava?",["1","3","4","ani jeden, netreba jej"],"4"),
     new Question("Medzi experimentálne potvrdené intermediálne častice nepatrí:",["Gluón","Fotón","Bozón","Gravitón"],"Gravitón"),
-    new Question("Aký je vek zeme?",["2019 rokov","4,5 mld rokov","Planckov čas","Ta nepamätám",],"4,5 mld rokov"),
-    new Question("Za predpokladu, že derivácia je smernica sečnice dotyčnice a v tieni je 19 stupňov, koľko žiakov má naša škola?",["100","cca 600","dva sto","300"],"cca 600"),
+    new Question("Aký je vek zeme?",["2019 rokov","4,5 mld rokov","Planckov čas","Neviem, tak dlho nežijem",],"4,5 mld rokov"),
+    new Question("Za predpokladu, že derivácia je smernica sečnice dotyčnice a v tieni je 19 stupňov, koľko žiakov má naša škola?",["100","cca 600","200","300"],"cca 600"),
     new Question("Ktorý z týchto jazykov nie je programovací?",["Python","C","Ruby","HTML"],"HTML")
 ]
 
